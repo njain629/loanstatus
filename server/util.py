cc4569336b46a69494c8e9a703168f0612f882d8
Naman jain <njain629@gmail.com>
2020-12-25 18:38:10 +0530

    Add files via upload

--- server/util.py
+++ server/util.py
@@ -27,7 +27,7 @@ def load_saved_artifacts():
 
     global __model
     if __model is None:
-        with open(artifacts[0]+"\pickle_loanData_model.pickle", 'rb') as f:
+        with open(artifacts[0]+"/pickle_loanData_model.pickle", 'rb') as f:
             __model = pickle.load(f)
     print("loading saved artifacts...done")
 
